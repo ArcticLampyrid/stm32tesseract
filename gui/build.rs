8bde7d66fc8452c58b15eb61eb868805a8ad1c61
ArcticLampyrid <ArcticLampyrid@outlook.com>
2023-11-01 20:42:59 +0800

    feat: set up app icon

--- gui/build.rs
+++ gui/build.rs
@@ -1,6 +1,16 @@
+use std::fs;
+
 use slint_build::CompilerConfiguration;
 
 fn main() {
     let config = CompilerConfiguration::new().with_style("fluent-dark".to_string());
     slint_build::compile_with_config("ui/appwindow.slint", config).unwrap();
+
+    // Embed the Windows resource file
+    if let Ok(paths) = fs::read_dir("win32_resource") {
+        for path in paths.flatten() {
+            println!("cargo:rerun-if-changed={}", path.path().display());
+        }
+    }
+    embed_resource::compile("win32_resource/app.rc", embed_resource::NONE);
 }
