763b4bdd020e9b95a917755d3e9d0a9d77b91daa
ArcticLampyrid <ArcticLampyrid@outlook.com>
2024-11-05 10:49:35 +0800

    perf: include more information in UA

--- cli/src/reqwest_unified_builder.rs
+++ cli/src/reqwest_unified_builder.rs
@@ -1,5 +1,8 @@
 pub fn build_blocking() -> Result<reqwest::blocking::Client, reqwest::Error> {
     reqwest::blocking::ClientBuilder::new()
-        .user_agent("stm32tesseract")
+        .user_agent(concat!(
+            "Mozilla/5.0 (Generic) STM32Tesseract/",
+            env!("CARGO_PKG_VERSION")
+        ))
         .build()
 }
