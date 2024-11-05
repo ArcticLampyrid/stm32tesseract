a515611090791f2a76976272540ab7c9ebd5f6ca
ArcticLampyrid <ArcticLampyrid@outlook.com>
2024-11-05 10:50:31 +0800

    fix: high availability for detecting china outbound

--- cli/src/gh_helper/mirror_election.rs
+++ cli/src/gh_helper/mirror_election.rs
@@ -9,39 +9,63 @@ enum MirrorType {
 }
 static MIRROR_TYPE: std::sync::OnceLock<MirrorType> = std::sync::OnceLock::new();
 
+fn is_outbound_to_china() -> bool {
+    let urls = [
+        "https://www.cloudflare.com/cdn-cgi/trace",
+        "https://www.cloudflare-cn.com/cdn-cgi/trace",
+        "https://www.cf-ns.com/cdn-cgi/trace",
+    ];
+    let client = reqwest_unified_builder::build_blocking();
+    if let Ok(client) = client {
+        for url in urls.iter() {
+            let result = client
+                .get(*url)
+                .timeout(Duration::from_secs(10))
+                .send()
+                .ok()
+                .and_then(|response| {
+                    if response.status().is_success() {
+                        response.text().ok().and_then(|body| {
+                            if body.contains("loc=") {
+                                return Some(body.contains("loc=CN"));
+                            }
+                            None
+                        })
+                    } else {
+                        None
+                    }
+                });
+            if let Some(result) = result {
+                return result;
+            }
+        }
+    }
+    false
+}
+
 fn get_mirror_type() -> MirrorType {
     let result = *MIRROR_TYPE.get_or_init(|| {
+        if !is_outbound_to_china() {
+            return MirrorType::Official;
+        }
         let client = reqwest_unified_builder::build_blocking();
         if let Ok(client) = client {
-            {
-                // check location
-                // for non-China users, use official resources
-                let response = client
-                    .get("https://www.cloudflare.com/cdn-cgi/trace")
-                    .timeout(Duration::from_secs(10))
-                    .send();
-                if let Ok(response) = response {
-                    if response.status().is_success() {
-                        if let Ok(body) = response.text() {
-                            if !body.contains("loc=CN") {
-                                return MirrorType::Official;
-                            }
-                        }
-                    }
-                }
-            }
             // for China users, there may be a challenge to download the official resources
-            {
-                // check if ghproxy is available
-                let response = client
-                    .get("https://ghp.ci/")
-                    .timeout(Duration::from_secs(10))
-                    .send();
-                if let Ok(response) = response {
+            // use proxy if available
+            let use_proxy = client
+                .get("https://ghp.ci/")
+                .timeout(Duration::from_secs(10))
+                .send()
+                .ok()
+                .map(|response| {
                     if response.status().is_success() || response.status().is_redirection() {
-                        return MirrorType::GhProxy;
+                        return Some(());
                     }
-                }
+                    None
+                })
+                .is_some();
+            if use_proxy {
+                return MirrorType::GhProxy;
             }
         }
         MirrorType::Official
